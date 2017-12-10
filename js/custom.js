3b14354d32b75ceadfe6124737bc3446da2a755b
Brittik Basu <brittikbasu@Brittiks-MacBook-Pro.local>
2017-12-10 11:28:13 +0530

    make cloud transition work

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,12 @@
 (function(){
 
     setTimeout(function(){ 
-document.getElementById("cloudup").className = "appear fadeInUpBig";
+document.getElementById("cloudup").className = "inner-link appear fadeInUpBig";
 console.log("animation 1 completed");
     }, 3300);
 
     setTimeout(function(){ 
-    document.getElementById("cloudup").className = "appear rubberBand";
+    document.getElementById("cloudup").className = "inner-link appear rubberBand";
     console.log("animation 2 completed");
     }, 5700);
 
