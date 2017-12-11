f2db52fed22de4b15e295032efb62965e0f41cef
Brittik Basu <brittikbasu@Brittiks-MacBook-Pro.local>
2017-12-11 11:17:35 +0530

    update animation duration

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,6 @@
     setTimeout(function(){ 
 document.getElementById("cloudup").className = "inner-link appear fadeInUpBig";
 console.log("animation 1 completed");
-    }, 4300);
+    }, 1300);
 
   })();
