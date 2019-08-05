aeff23752c5b703d89db75960d2d35a97f1f3d74
Brittik basu <brittikbasu@gmail.com>
2019-08-05 14:36:29 +0530

    Revert "remove partner link from nav"
    This reverts commit a6208e6bd4f3f6e2ea2510411af86ed8e1880f38.

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// (function(){
+
+//     setTimeout(function(){ 
+//         document.getElementById("ipad").className = "appear fadeInDown";
+//         console.log("animation 0 completed");
+//             }, 600);
+
+//    /* setTimeout(function(){ 
+// document.getElementById("cloudup").className = "inner-link appear fadeInUpBig";
+// console.log("animation 1 completed");
+//     }, 1300);
+// */
+//   })();
