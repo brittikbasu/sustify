ae0f6b50c622b0d2c7c5c593150102ed38320c0d
brittik basu <brittikbasu@gmail.com>
2020-01-13 09:05:06 +0530

    Revert "Merge pull request #19 from brittikbasu/master"
    This reverts commit 5a32f9959f392bb11970ffedefa8494250e0f7a2, reversing
    changes made to 7aafce5f6b155763fb565df75c6eb74667820804.

--- js/custom.js
+++ js/custom.js
@@ -1,51 +1,13 @@
 // (function(){
 
-//     setTimeout(function(){
+//     setTimeout(function(){ 
 //         document.getElementById("ipad").className = "appear fadeInDown";
 //         console.log("animation 0 completed");
 //             }, 600);
 
-//    /* setTimeout(function(){
+//    /* setTimeout(function(){ 
 // document.getElementById("cloudup").className = "inner-link appear fadeInUpBig";
 // console.log("animation 1 completed");
 //     }, 1300);
 // */
 //   })();
-
-/* delina logo appears after scroll 
-var delina = () => {
-  if (window.innerWidth >= 768 && window.scrollY > 923) {
-    document.querySelector(".delina-desktop").style.display = "inline";
-  } else {
-    document.querySelector(".delina-desktop").style.display = "none";
-  }
-
-  if (
-    window.innerWidth < 768 &&
-    window.scrollY >= 745 &&
-    window.scrollY <= 9569
-  ) {
-    document.querySelector(".delina-mobile").style.display = "inline";
-  } else {
-    document.querySelector(".delina-mobile").style.display = "none";
-  }
-};
-
-window.addEventListener("scroll", delina);
-window.onload(delina);
-*/
-
-//window.addEventListener("scroll", delina);
-window.onload = () => {
-  if (window.innerWidth >= 768) {
-    document.querySelector(".delina-desktop").style.display = "inline";
-  } else {
-    document.querySelector(".delina-desktop").style.display = "none";
-  }
-
-  if (window.innerWidth < 768) {
-    document.querySelector(".delina-mobile").style.display = "inline";
-  } else {
-    document.querySelector(".delina-mobile").style.display = "none";
-  }
-};
