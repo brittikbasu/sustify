39959253c2e35f9a78dda8e8dec282fce46e0f70
brittik basu <brittikbasu@gmail.com>
2020-01-13 09:13:12 +0530

    load delina banner onload

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,13 @@
-// (function(){
+window.onload = () => {
+  if (window.innerWidth >= 768) {
+    document.querySelector(".delina-desktop").style.display = "inline";
+  } else {
+    document.querySelector(".delina-desktop").style.display = "none";
+  }
 
-//     setTimeout(function(){ 
-//         document.getElementById("ipad").className = "appear fadeInDown";
-//         console.log("animation 0 completed");
-//             }, 600);
-
-//    /* setTimeout(function(){ 
-// document.getElementById("cloudup").className = "inner-link appear fadeInUpBig";
-// console.log("animation 1 completed");
-//     }, 1300);
-// */
-//   })();
+  if (window.innerWidth < 768) {
+    document.querySelector(".delina-mobile").style.display = "inline";
+  } else {
+    document.querySelector(".delina-mobile").style.display = "none";
+  }
+};
